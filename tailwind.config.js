138487711e870bef3e2d51b1b52dd35d5494e898
alphakahere <alphakahere@gmail.com>
2022-10-22 07:52:35 +0000

    create menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ["./assets/**/*.js", "./templates/**/*.html.twig"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dark: "#12121e",
+				"dark-100": "#292e41",
+				"dark-200": "#080a31",
+				"dark-300": "#070724",
+				"dark-400": "#030312",
+				"metablue-100": "#17c8f6",
+				"metablue-200": "#319df5",
+				"metablue-300": "#3f87f3",
+				"metablue-400": "#4d75f4",
+			},
+		},
 	},
 	plugins: [],
 };
